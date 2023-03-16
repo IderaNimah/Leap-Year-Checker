a725c0e70f5d06fb9bacf0439c454ced1d9e91eb
IderaNimah <iderayusuf29@gmail.com>
2023-03-16 20:32:24 +0100

    project done? maybe

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ const result = document.querySelector('#result');
 
 // Add an event listener to the form for when it is submitted
 form.addEventListener('submit', function(event) {
-  // Prevent the default form submission behavior
+// Prevent the default form submission behavior
   event.preventDefault();
 
-  // Get the value of the input element and convert it to a number
+// Get the value of the input element and convert it to a number
   const year = Number(yearInput.value);
 
-  // Check if the entered year is a leap year
+// Check if the entered year is a leap year
   if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
-    // Update the result element with a message indicating that the year is a leap year
-    result.textContent = ${year} is a leap year.;
+// Update the result element with a message indicating that the year is a leap year
+    result.textContent =  `${year} is a leap year`;
   } else {
-    // Update the result element with a message indicating that the year is not a leap year
-    result.textContent = ${year} is not a leap year.;
+// Update the result element with a message indicating that the year is not a leap year
+    result.textContent = `${year} is not a leap year`;
   }
 });
